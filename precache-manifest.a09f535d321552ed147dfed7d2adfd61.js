8d6a4592493835b2cc09fc039c35e929cb52eb98
oswin-jerome <oswinjeromej@gmail.com>
2020-08-16 20:19:55 +0530

    Updates

---
+++ precache-manifest.a09f535d321552ed147dfed7d2adfd61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e82d80ba2074da88259062b870bdab6",
+    "url": "/WhiTe/index.html"
+  },
+  {
+    "revision": "65dc6eb43fd11668ce3c",
+    "url": "/WhiTe/static/css/main.5bb79c08.chunk.css"
+  },
+  {
+    "revision": "0831ffd81011f356c051",
+    "url": "/WhiTe/static/js/2.52260be8.chunk.js"
+  },
+  {
+    "revision": "a8eb76e4c6beb72c22bcc9f3c026930b",
+    "url": "/WhiTe/static/js/2.52260be8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65dc6eb43fd11668ce3c",
+    "url": "/WhiTe/static/js/main.45edc1fc.chunk.js"
+  },
+  {
+    "revision": "4194991c3e327ad6b432",
+    "url": "/WhiTe/static/js/runtime-main.25bebb42.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/WhiTe/static/media/logo.5d5d9eef.svg"
+  }
+]);
