d76304d2358b62f51e5a5f0011f53b7830193376
oswin-jerome <oswinjeromej@gmail.com>
2020-08-17 20:57:52 +0530

    Footer bug fix

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,8 +4,8 @@ const Footer = () => {
     return ( 
         <footer className="bg-dark text-white pl-lg-5 pl-sm-0 py-4">
             <div className="row container-fluid">
-                <p className="col-6">© whitechnalogies.in</p>
-                <p className="col-6 text-right">Designed and developed by <span className="bg-warning py-1 px-2 rounded-lg">Oswin Jerome</span></p>
+                <p className="col-lg-6 col-sm-12">© whitechnalogies.in</p>
+                <p className="col-lg-6 col-sm-12 text-sm-left text-lg-right ">Designed and developed by <span className="bg-warning py-1 px-2 rounded-lg">Oswin Jerome</span></p>
             </div>
         </footer>
     );
